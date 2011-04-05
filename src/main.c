97328ff6528a321d13ef0d1119d338c4a2b78959
Nis Sarup <nis@sarup.dk>
2011-04-05 09:46:28 +0200

    Got the LEDs to toggle on up-click. After 5 faultISR in a row. Stupid RedSuite.

--- src/main.c
+++ src/main.c
@@ -11,6 +11,10 @@
 #include "Task.h"
 #include "queue.h"
 #include "semphr.h"
+#include "cpu/cpu.h"
+#include "led/led.h"
+//#include "pot/pot.h"
+#include "buttons/buttons.h"
 
 #define USERTASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
@@ -19,7 +23,12 @@ void __error__(char *pcFilename, unsigned long ulLine) {
 
 static void setupHardware(void) {
 	// TODO: Put hardware configuration and initialisation in here
-	
+	disable_global_int();
+	clk_system_init();
+	init_leds();
+	//init_pot();
+	init_buttons();
+	enable_global_int();
 	// Warning: If you do not initialize the hardware clock, the timings will be inaccurate
 }
 
@@ -27,28 +36,33 @@ static void setupHardware(void) {
  * Simple task that just toggles between to states
  */
 void vUserTask1(void *pvParameters) {
-	static int iState = 0;
 
 	while (1) {
-		if (iState == 0) {
-			iState = 1;
-		} else {
-			iState = 0;
+		
+		button_task();
+		
+		INT8U up_clicks = get_up_clicks();
+		INT8U down_clicks = get_down_clicks();
+		
+		if(up_clicks)
+		{
+			led_all_toggle();
 		}
-		vTaskDelay(100);
+		
+		vTaskDelay(20);
 	}
 }
 
 /**
  * Simple task that increments a counter
  */
-void vUserTask2(void *pvParameters) {
-	int count = 0;
-	while (1) {
-		count++;
-		vTaskDelay(101) ;
-	}
-}
+// void vUserTask2(void *pvParameters) {
+// 	int count = 0;
+// 	while (1) {
+// 		count++;
+// 		vTaskDelay(101) ;
+// 	}
+// }
 
 /**
  * Program entry point 
@@ -60,7 +74,7 @@ int main(void) {
 	 * Start the tasks defined within this file/specific to this demo. 
 	 */
 	xTaskCreate( vUserTask1, ( signed portCHAR * ) "Task1", USERTASK_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL );
-	xTaskCreate( vUserTask2, ( signed portCHAR * ) "Task2", USERTASK_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL );
+	//xTaskCreate( vUserTask2, ( signed portCHAR * ) "Task2", USERTASK_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL );
 
 	/* 
 	 * Start the scheduler. 
